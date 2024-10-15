f431bb7eb4a50f15d2b374ca4512ba1a41f95bcb
duckhy04 <nguynduchuy12@gmail.com>
2024-10-16 02:47:46 +0700

    Update spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { MaterialAngularModules } from './shared/modules/MaterialAngularModules';
 import { Modules } from './shared/modules/ImportModules';
-import { UserStorageService } from './core/storage/user-storage.service';
 import { SpinnerComponent } from "./shared/components/spinner/spinner.component";
 
 @Component({
@@ -14,4 +13,19 @@ import { SpinnerComponent } from "./shared/components/spinner/spinner.component"
 })
 export class AppComponent {
   title = 'cyber-guardian-frontend';
+  loading: boolean = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    // Bắt sự kiện điều hướng để hiển thị spinner khi chuyển trang
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.loading = true; // Hiển thị spinner khi bắt đầu điều hướng
+      }
+      if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
+        this.loading = false; // Ẩn spinner khi điều hướng hoàn tất hoặc bị hủy
+      }
+    });
+  }
 }
